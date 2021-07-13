0c5a07e1db84539098283697e4c53ef832f79768
EdlaineNunes <55089537+EdlaineNunes@users.noreply.github.com>
2021-07-13 10:52:19 -0300

    Add files via upload

---
+++ go/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func greet(prefix, name string){
+	fmt.Printf("%s %s!\n", prefix, name)
+}
+
+func formatGreet(prefix, name string) string{
+	return prefix + " " + name + "! \n"
+}
+
+
+func main() {
+	fmt.Printf("Hello World!\n")
+	fmt.Printf("Time now: %s\n", time.Now())
+	greet("Hello", "Edlaine")
+	greet("Hello", "World!")
+	fmt.Printf(formatGreet("Imprimir", "formatado!"))
+}
